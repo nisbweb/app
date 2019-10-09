16e67057fdaa69251138491740806d75f215e426
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-09 17:01:39 +0000

    Deploy nisbweb/app to github.com/nisbweb/app.git:gh-pages

---
+++ precache-manifest.fc166dc9f89c6c62de47b605f7c7628e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/app/robots.txt"
+  },
+  {
+    "revision": "4473ddf2a9a51882f8eb",
+    "url": "/app/js/chunk-vendors.3b390473.js"
+  },
+  {
+    "revision": "7fecf795e7969732550e",
+    "url": "/app/js/app.1baa8e73.js"
+  },
+  {
+    "revision": "90d7d54957b679e3315e58e6e70c67da",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "4473ddf2a9a51882f8eb",
+    "url": "/app/css/chunk-vendors.25824717.css"
+  },
+  {
+    "revision": "7fecf795e7969732550e",
+    "url": "/app/css/app.2d8419ee.css"
+  }
+];
